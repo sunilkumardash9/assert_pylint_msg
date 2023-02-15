6244c669ceaba12678100a27029705547c835667
Sunil Kumar Dash <47926185+sunilkumardash9@users.noreply.github.com>
2023-02-15 12:11:24 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-s = 'hello world'
-print('hello world')
+self.post_json('/ml/\\trainedclassifierhandler', 
+                self.payload, expect_errors=True, expected_status_int=401)
+if (a>b and 
+       b>a):pass
